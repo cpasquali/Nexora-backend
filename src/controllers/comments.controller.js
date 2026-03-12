b4f81c001bcde635963973b509f25b37b3fc5b57
cpasquali <conipasquali2@gmail.com>
2026-03-12 20:23:15 -0300

    se agrego la funcion para formatear el created_at del comentario

--- src/controllers/comments.controller.js
+++ src/controllers/comments.controller.js
@@ -1,19 +1,41 @@
 import { Comment, User } from "../models/Associations.js";
 import jwt from "jsonwebtoken";
 
+function timeAgo(date) {
+  const now = new Date();
+  const seconds = Math.floor((now - new Date(date)) / 1000);
+
+  const minutes = Math.floor(seconds / 60);
+  const hours = Math.floor(seconds / 3600);
+  const days = Math.floor(seconds / 86400);
+  const weeks = Math.floor(seconds / 604800);
+
+  if (seconds === 0) return "now";
+  if (seconds < 60) return `${seconds}s`;
+  if (minutes < 60) return `${minutes}m`;
+  if (hours < 24) return `${hours}h`;
+  if (days < 7) return `${days}d`;
+  return `${weeks}w`;
+}
+
 export const getCommentsByPost = async (req, res) => {
   try {
     const { postId } = req.params;
 
-    const comments = await Comment.findAll({
+    const commentsDb = await Comment.findAll({
       where: { post_id: postId },
       include: {
         model: User,
         as: "user",
-        attributes: ["full_name", "username", "image_url"],
+        attributes: ["full_name", "username", "image_url", "created_at"],
       },
       order: [["created_at", "DESC"]],
     });
+
+    const comments = commentsDb.map((comment) => ({
+      ...comment.toJSON(),
+      time_ago: timeAgo(comment.created_at),
+    }));
 
     if (comments.length === 0) return res.status(200).json({ comments: [] });
 
@@ -38,8 +60,15 @@ export const createComment = async (req, res) => {
       user_id,
     });
 
-    return res.status(201).json({ message: "Comment created", newComment });
+    return res.status(201).json({
+      message: "Comment created",
+      newComment: {
+        ...newComment.toJSON(),
+        time_ago: timeAgo(new Date()),
+      },
+    });
   } catch (error) {
-    res.status(500).json({ message: error.message });
+    console.log(error.message);
+    return res.status(500).json({ message: error.message });
   }
 };
--- src/models/Comment.model.js
+++ src/models/Comment.model.js
@@ -22,6 +22,10 @@ export const Comment = db.define(
       type: DataTypes.INTEGER,
       allowNull: false,
     },
+    created_at: {
+      type: DataTypes.TIME,
+      allowNull: true,
+    },
   },
   { timestamps: false },
 );
